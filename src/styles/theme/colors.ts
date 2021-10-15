949ff95248d0cb06c657aeb7e428111550f7473a
vitorserrano <vitorcirinoserrano@gmail.com>
2021-10-14 22:54:13 -0300

    freat: add global theme

---
+++ src/styles/theme/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  lightGreen: '#36E61A',
+  darkGreen: '#28C70F',
+  white: '#FFFFFF',
+  background: '#F1F1F1',
+  shadow: 'box-0px 0px 6px 4px rgba(245, 251, 255, 0.6)',
+}
---
+++ src/styles/theme/fonts.ts
@@ -0,0 +1,5 @@
+export const fonts = {
+  text400: 'Roboto_400Regular',
+  text500: 'Roboto_500Medium',
+  title600: 'Roboto_700Bold',
+}
