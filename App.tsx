bd43565826c101ff95e3164696743b2534a09c0d
vitorserrano <vitorcirinoserrano@gmail.com>
2021-10-14 22:54:52 -0300

    feat: add fonts with expo hook

--- App.tsx
+++ App.tsx
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+import { StatusBar } from 'expo-status-bar'
+import AppLoading from 'expo-app-loading'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { ThemeProvider } from 'styled-components'
+
+import { appTheme } from './src/styles/theme'
+import { Products } from './src/pages/Products'
+
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={appTheme}>
+      <StatusBar style="light" />
+      <Products />
+    </ThemeProvider>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+export default App
