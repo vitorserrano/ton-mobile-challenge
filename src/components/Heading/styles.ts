8bfc099448164b8d772c6a27021255c6631cef45
vitorserrano <vitorcirinoserrano@gmail.com>
2021-10-16 20:30:13 -0300

    feat: create heading component

---
+++ src/components/Heading/styles.ts
@@ -0,0 +1,10 @@
+import styled, { css } from 'styled-components/native'
+
+export const Text = styled.Text`
+  ${({ theme: { colors, fonts } }) => css`
+    color: ${colors.heading};
+    font-family: ${fonts.heading700};
+    font-size: 24px;
+    margin-top: 40px;
+  `}
+`
