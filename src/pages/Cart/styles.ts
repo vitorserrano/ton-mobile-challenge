a49180e53b660234c8ed581404058e632036d120
vitorserrano <vitorcirinoserrano@gmail.com>
2021-10-17 20:29:32 -0300

    feat: finish cart screen

--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -4,8 +4,10 @@ import { FlatList } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
 import { CartItemProps } from '../../context/cart'
+import { bottomSpaceHeight } from '../../utils/constants'
 
 export const Container = styled.View`
+  flex: 1;
   padding: 0 24px;
   margin-top: 40px;
 `
@@ -23,7 +25,9 @@ export const BackButton = styled(Feather).attrs({
 
 export const CartItemList = styled(
   FlatList as new () => FlatList<CartItemProps>
-)``
+)`
+  margin-top: 10px;
+`
 
 export const EmptyContainer = styled.View`
   flex: 1;
@@ -35,7 +39,7 @@ export const EmptyImageContainer = styled.View`
   width: 240px;
   height: 240px;
   border-radius: 22px;
-  background-color: #e4e4e4;
+  background-color: ${({ theme }) => theme.colors.cover};
   justify-content: center;
   align-items: center;
   margin-bottom: 24px;
@@ -55,5 +59,41 @@ export const Description = styled.Text`
     line-height: 22px;
     margin-top: 8px;
     text-align: center;
+  `}
+`
+
+export const AllProductsInfo = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: 24px;
+  padding-bottom: ${bottomSpaceHeight};
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+`
+
+export const AllProductsText = styled.Text`
+  ${({ theme: { colors, fonts } }) => css`
+    color: ${colors.heading};
+    font-family: ${fonts.heading700};
+    font-size: 18px;
+    line-height: 24px;
+    margin-left: 6px;
+  `}
+`
+
+export const BuyButton = styled.TouchableOpacity`
+  padding: 14px 24px;
+  background-color: ${({ theme }) => theme.colors.green500};
+  border-radius: 8px;
+`
+
+export const BuyButtonText = styled.Text`
+  ${({ theme: { colors, fonts } }) => css`
+    color: ${colors.white};
+    font-family: ${fonts.heading700};
+    font-size: 16px;
   `}
 `
